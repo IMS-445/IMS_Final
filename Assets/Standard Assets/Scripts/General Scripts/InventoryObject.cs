cdb874d9b7534b790edf8fd7e1c3d5600141ea28
Matthew Dwyer <dwyermw@muohio.edu>
2014-10-21 15:47:19 -0400

    Comments in InventoryObject

--- Assets/Standard Assets/Scripts/General Scripts/InventoryObject.cs
+++ Assets/Standard Assets/Scripts/General Scripts/InventoryObject.cs
@@ -14,39 +14,41 @@ public class InventoryObject {
 	}
 
 	public InventoryObject(int qty, string name){
-				this.quantity = qty;
-				this.name = name;
+		this.quantity = qty;
+		this.name = name;
 	}
 
+	// Edit the quantity by the signified amount
 	public void editQty(int update)
 	{
 		quantity += update;
 	}
 
+	// Get the quantity of an object
 	public int getQty()
 	{
 		return quantity;
 	}
 
 	// Not in here, in char controller
-	public bool interact(GameObject obj)
-	{
-		switch(obj.tag)
-		{
-		case "fire" : 
-			InventoryObject io = uinventory.Find(i => i.name == "full_bucket" && i.quantity > 0);
-			if(io != null){
-				Destroy(obj);
-				io.editQty(-1);
-				if(io.getQty <= 0)
-					uinventory.Remove(i => i.name == "full_bucket");
-				InventoryObject bucket = uinventory.Find(i => i.name == "empty_bucket");
-				if(bucket != null)
-					bucket.editQty(1);
-				else
-					uinventory.Add(new InventoryObject("empty_bucket", 1));
-			}
-		}
-	}
+//	public bool interact(GameObject obj)
+//	{
+//		switch(obj.tag)
+//		{
+//		case "fire" : 
+//			InventoryObject io = uinventory.Find(i => i.name == "full_bucket" && i.quantity > 0);
+//			if(io != null){
+//				Destroy(obj);
+//				io.editQty(-1);
+//				if(io.getQty <= 0)
+//					uinventory.Remove(i => i.name == "full_bucket");
+//				InventoryObject bucket = uinventory.Find(i => i.name == "empty_bucket");
+//				if(bucket != null)
+//					bucket.editQty(1);
+//				else
+//					uinventory.Add(new InventoryObject("empty_bucket", 1));
+//			}
+//		}
+//	}
 }
 
