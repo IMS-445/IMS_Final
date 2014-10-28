557806920425064428611984a13c9de95bf879dd
Matthew Dwyer <dwyermw@muohio.edu>
2014-10-28 15:45:39 -0400

    Added Game Over scene

---
+++ Assets/GameOverTextScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverTextScript : MonoBehaviour {
+	public GUIText msg;
+	public GameObject manager;
+	// Use this for initialization
+	void Start () {
+		//msg.text = "Game Over\nNumber of civilians saved: " + manager.civiliansSaved;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
