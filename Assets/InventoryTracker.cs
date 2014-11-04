aea95df21c9abd3625961169391afec4da453b90
Amber W <wolfercm@miamioh.edu>
2014-11-04 13:56:43 -0500

    Added Inventory Tracker.  Changed the location of the fire.

---
+++ Assets/InventoryTracker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class InventoryTracker : MonoBehaviour {
+
+	public List<InventoryObject> playerInventory;
+	public GUIText inventoryList;
+	// Use this for initialization
+	void Start () {
+		GameObject PlayerChar = GameObject.FindGameObjectWithTag ("Player");
+		playerInventory = (PlayerChar.GetComponent<ThirdPersonController> ()).inventory;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string s ="";
+		foreach (InventoryObject io in playerInventory) {
+			s += io.name + " x" + io.getQty() + "\r\n";
+		}
+		inventoryList.text = s;
+	}
+}
