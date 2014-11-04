1db197833a10742f6ba443f1c4a64c09926906ce
Matthew Dwyer <dwyermw@muohio.edu>
2014-11-04 14:58:20 -0500

    Menu scene updated

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -6,7 +6,7 @@ public class ButtonScript : MonoBehaviour {
 	void OnGUI () {
 		GUIStyle style = new GUIStyle ();
 		if (GUI.Button (new Rect (155, 200, 100, 30), "New Game")) {
-			Application.LoadLevel("Start");
+			Application.LoadLevel("BaseLevel");
 		}
 	}
 }
