083117b50133df99dd5175fb1dd23de73a1cca22
Matthew Dwyer <dwyermw@muohio.edu>
2014-11-04 02:44:18 -0500

    Tree assets added, trees added to base level

--- Assets/Scripts/GameOverTextScript.cs
+++ Assets/Scripts/GameOverTextScript.cs
@@ -3,11 +3,10 @@ using System.Collections;
 
 public class GameOverTextScript : MonoBehaviour {
 	public GUIText msg;
-	public GameObject manager;
 	// Use this for initialization
 	void Start () {
-		manager = GameObject.FindGameObjectWithTag ("GameController");
-		//msg.text = "Game Over\nNumber of civilians saved: " + manager.civiliansSaved;
+		//manager = GameObject.FindGameObjectWithTag ("GameController");
+		msg.text = "Number of civilians saved: " + GameController.control.getCiviliansSaved();
 	}
 	
 	// Update is called once per frame
