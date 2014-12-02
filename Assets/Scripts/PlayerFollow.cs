8ed91d947ae448f5d12bcbb44859eaf005adeb38
Amber W <wolfercm@miamioh.edu>
2014-12-02 15:50:02 -0500

    Added a drag script and made minor changes to level 2 and level 3.

---
+++ Assets/Scripts/PlayerFollow.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFollow : MonoBehaviour {
+
+	public GameObject PlayerChar;
+	public GameObject followObject;
+	public static PlayerFollow playerFollow;
+
+	void Awake() {
+		if (playerFollow == null) {
+			playerFollow = this;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		PlayerChar = GameObject.FindGameObjectWithTag ("PlayerFollow");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(followObject != null)
+		{
+			Vector3 idealPosition = PlayerChar.transform.position;
+			Vector3 currentPoisition = followObject.gameObject.transform.position;
+			float dist = (10f - Vector3.Distance(currentPoisition,idealPosition)) / 10f;
+			if(dist > .95f){
+				dist = 1f;
+			}else{
+				dist = .05f;
+			}
+			followObject.gameObject.transform.position = Vector3.Lerp(currentPoisition,idealPosition,dist);
+			//print ( currentPoisition + " " + idealPosition + "  " + dist );
+		}
+	}
+
+	public void SetAsFollow(GameObject newFollow){
+		followObject = newFollow;
+	}
+
+	public GameObject getFollow(){
+		return followObject;
+	}
+}
