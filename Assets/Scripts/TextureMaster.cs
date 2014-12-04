d2956c6a2d66145fa0771f368d876c5a52baaa92
Amber W <wolfercm@miamioh.edu>
2014-12-03 23:42:02 -0500

    Added new Scenes and stuff

---
+++ Assets/Scripts/TextureMaster.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureMaster : MonoBehaviour {
+
+	public Texture[] Scenes;
+	public int counter;
+	public string NextScene;
+	public GUITexture TextureToShow;
+	public double wait;
+
+
+	// Use this for initialization
+	void Start () {
+		counter = 0;
+		if (Scenes.Length == counter) {
+			Application.LoadLevel(NextScene);
+		} else {
+			TextureToShow.texture = Scenes[counter];
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		wait = wait - Time.deltaTime;
+		if (wait < 0) {
+			wait = 0.0f;
+		}
+		if(Input.anyKeyDown && wait <= 0.0f){
+			wait = 5.0f;
+			counter++;
+			if (Scenes.Length == counter) {
+				Application.LoadLevel(NextScene);
+			} else {
+				TextureToShow.texture = Scenes[counter];
+			}
+		}
+	}
+}
